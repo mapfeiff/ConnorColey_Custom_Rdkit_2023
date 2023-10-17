217e962567c8b263a661dc733f71b7ac1a495159
mapfeiff <mapfeiff@uci.edu>
2023-10-17 16:56:06 -0700

    trying out solution of using the up-to-date imports in RDGeneral/LocaleSwitcher. Doing this because previous errors with the includes could be causing file to be not found and thus, deleted

--- Code/RDGeneral/LocaleSwitcher.cpp
+++ Code/RDGeneral/LocaleSwitcher.cpp
@@ -28,19 +28,24 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
+#if defined(__CYGWIN__) && !defined(_GNU_SOURCE)
+// -std=c++11 turns off recent POSIX features!
+#define _GNU_SOURCE
+#endif
+
 #include "LocaleSwitcher.h"
+#include <string>
 
 // LocaleSwitcher Dependencies
 #include <clocale>
-#ifndef _WIN32
-// #include <xlocale.h> --DEPRICATED--
-#include <string>
-#else
+#ifdef __APPLE__
+#include <xlocale.h>
+#endif
+#ifdef __CYGWIN__
 #include <locale.h>
 #endif
-
-#ifdef RDK_THREADSAFE_SSS
-#include <boost/thread/tss.hpp>
+#ifdef RDK_BUILD_THREADSAFE_SSS
+#include <thread>
 #endif
 
 namespace RDKit {
